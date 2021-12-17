db7727d47fef1ca3881ccd2989c41a4678113645
hirata-keisuke <plainpeace39th@gmail.com>
2021-12-18 00:34:59 +0900

    models.pyを追加 #2

--- app/models.py
+++ app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+
+    SEX = [("M", "男性"),["F", "女性"]]
+
+    name = models.CharField("会員の名前", max_length=20)
+    user_name = models.CharField("会員のユーザー名", max_length=20)
+    sex = models.CharField("会員の性別", max_length=1, choices=SEX)
+    password = models.CharField("会員のパスワード", max_length=15)
+    address = models.CharField("会員の出身地", max_length=30)
+    birth_date = models.DateField("会員の生年月日")
+    mail = models.EmailField("会員のeメールアドレス")
+
+class BeerStyle(models.Model):
+
+    style = models.CharField("ビアスタイルの名称", max_length=20)
+    birthplace = models.CharField("ビアスタイルの発祥地", max_length=20)
+    fermentation = models.CharField("醸造方法", max_length=10)
+    brands = models.ManyToManyField(BeerBrand)
+
+class BeerBrand(models.Model):
+
+    name = models.CharField("ビールの銘柄", max_length=15)
+    degree = models.FloatField("アルコール度数")
+    color = models.CharField("ビールの色", max_length=10)
+    srm = models.FloatField("米国醸造化学者学会によるビールの色の数値指標")
+    bitterness = models.FloatField("ビールの苦味IBU")
+    like = models.IntegerField("この銘柄を好きと言った会員数")
+    dislike = models.IntegerField("この銘柄を好きと言った会員数")
+
+class BeerReview(models.Model):
+
+    user = models.ForeignKey(Member, on_delete=models.CASCADE)
+    brand = models.ForeignKey(BeerBrand, on_delete=models.CASCADE)
+    review = models.TextField("ビール銘柄に関するレビュー文")
+    rate = models.IntegerField("会員の好み", choices=[(1, "好き"),(-1, "イマイチ")])
