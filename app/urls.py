1f315fe10c89402111dd7930a2b5b7b1d9158588
hirata-keisuke <plainpeace39th@gmail.com>
2021-12-22 01:37:31 +0900

    初めてのHTML追加

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("overview/", views.overview, name="overview"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import BeerBrand
 
 def index(request):
     return HttpResponse("Hello, world. You're at the app index.")
+
+def overview(request):
+    brand_list = BeerBrand.objects.all()
+    context = {"brand_list":brand_list}
+    return render(request, "app/overview.html", context)
